60039a6225f9e5b63ac7bf1069fe8a37217f9951
Homer Liu <kingliu06@gmail.com>
2023-01-15 18:06:18 +0800

    change to while x > 0

--- password_retry.py
+++ password_retry.py
@@ -1,7 +1,7 @@
 ans = 'a123456'
 x = 3 #剩餘機會
 
-while True:
+while x > 0:
 	pwd = input ('請輸入密碼:')
 
 	if  pwd == ans:
@@ -10,6 +10,4 @@ while True:
 	else:
 		x = x - 1 #嘗試錯誤後少一次機會
 		print ('密碼錯誤!還有',x , '次機會')
-		if x == 0:
-			print ('請15分鐘後再嘗試')
-			break
+		print ('請15分鐘後再嘗試')
